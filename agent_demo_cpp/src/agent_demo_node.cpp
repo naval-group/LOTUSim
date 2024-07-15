62bb69fa0d0e74d7d0162cae275fc9f420f14786
Cedric Buche <cedric.buche@pacific.naval-group.com>
2024-07-15 17:28:43 +0930

    updated the demo agent

--- agent_demo_cpp/src/agent_demo_node.cpp
+++ agent_demo_cpp/src/agent_demo_node.cpp
@@ -1,6 +1,12 @@
+#include "liquidai_msgs/msg/add_entity.hpp"
+#include "liquidai_msgs/srv/add_entity_srv.hpp"
+#include "liquidai_msgs/srv/add_entity_srv_array.hpp"
+#include "liquidai_msgs/srv/remove_entity.hpp"
 #include <chrono>
 #include <cstdio>
 #include <functional>
+#include <geometry_msgs/msg/point.hpp>
+#include <geometry_msgs/msg/vector3.hpp>
 #include <liquidai_msgs/msg/entity_position.hpp>
 #include <memory>
 #include <rclcpp/create_timer.hpp>
@@ -8,12 +14,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/int32.hpp>
 #include <string>
-#include <geometry_msgs/msg/point.hpp>
-#include <geometry_msgs/msg/vector3.hpp>
-#include "liquidai_msgs/msg/add_entity.hpp"
-#include "liquidai_msgs/srv/add_entity_srv.hpp"
-#include "liquidai_msgs/srv/add_entity_srv_array.hpp"
-#include "liquidai_msgs/srv/remove_entity.hpp"
 
 class AgentDemoCpp : public rclcpp::Node {
 public:
@@ -24,16 +24,21 @@ public:
         get_parameter<int>("gazebo_id", gazebo_id);
 
         pose_pub_ = this->create_publisher<liquidai_msgs::msg::EntityPosition>(
-            "/GazeboPosition", 100);
-
+            "/effector_demo_sched", 100);
         debug_pub_ = this->create_publisher<std_msgs::msg::Int32>(
             "/agent_demo_sched", 100);
 
-        timer_ = rclcpp::create_timer(
-            this,
-            this->get_clock(),
-            std::chrono::milliseconds(500),
-            std::bind(&AgentDemoCpp::timer_callback, this));
+        sensor_sub_ = this->create_subscription<std_msgs::msg::Int32>(
+            "/sensor_demo_sched",
+            10,
+            std::bind(
+                &AgentDemoCpp::sensor_callback, this, std::placeholders::_1));
+
+        // timer_ = timer_ = rclcpp::create_timer(
+        //     this,
+        //     this->get_clock(),
+        //     std::chrono::milliseconds(500),
+        //     std::bind(&AgentDemoCpp::timer_callback, this));
     }
 
 public:
@@ -57,11 +62,40 @@ public:
         dbg_msg.data = gazebo_id;
         debug_pub_->publish(dbg_msg);
     }
+
+    void sensor_callback(const std_msgs::msg::Int32::SharedPtr msg)
+    {
+        std_msgs::msg::Int32 dbg_msg;
+        dbg_msg.data = 0;
+        debug_pub_->publish(dbg_msg);
+        dbg_msg.data = gazebo_id;
+        debug_pub_->publish(dbg_msg);
+
+        RCLCPP_INFO(this->get_logger(), "Faking work on id %d", gazebo_id);
+        get_clock()->sleep_for(std::chrono::milliseconds(400));
+        auto message = liquidai_msgs::msg::EntityPosition();
+        message.id = gazebo_id;
+        x = x + 0.1;
+        message.position.set__x(x);
+        RCLCPP_INFO(
+            this->get_logger(),
+            "Publishing: '%s' on id %d",
+            std::to_string(message.position.x).c_str(),
+            gazebo_id);
+        pose_pub_->publish(message);
+
+        debug_pub_->publish(dbg_msg);
+        dbg_msg.data = 0;
+        debug_pub_->publish(dbg_msg);
+    }
+
+private:
     float x = 0;
     int gazebo_id;
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<liquidai_msgs::msg::EntityPosition>::SharedPtr pose_pub_;
     rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr debug_pub_;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sensor_sub_;
 };
 
 int main(int argc, char **argv)
