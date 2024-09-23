cacc2af7c87f7d460fcbeb557c940df74d94cfe5
Hélène Lechêne <hlechene.exterieur@pacific.naval-group.com>
2024-09-23 12:05:31 +0930

    harmonizing ports

--- launch-xdynSurface.sh
+++ launch-xdynSurface.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH="$(pwd)/physics/xdynSurface/"
 cd physics/xdynSurface
-clear;./xdyn-for-cs ../../assets/models/test_ship/testship-nowave-static.yml -v -a 127.0.0.1 -p 12346 -d --dt 0.2
+clear;./xdyn-for-cs ../../assets/models/test_ship/testship-nowave-static.yml -v -a 127.0.0.1 -p 12345 -d --dt 0.2
--- launch-xdynUnderwater.sh
+++ launch-xdynUnderwater.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH="$(pwd)/physics/xdynUnderwater"
 cd physics/xdynUnderwater
-clear;./xdyn-for-cs ../../assets/models/lrauv_xdyn/lrauv_old.yml -v -a 127.0.0.1 -p 12345 -d --dt 0.2
+clear;./xdyn-for-cs ../../assets/models/lrauv_xdyn/lrauv_old.yml -v -a 127.0.0.1 -p 12346 -d --dt 0.2
