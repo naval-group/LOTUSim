bd245520aeeb78eba464ddccb45211df0ea0a9e9
Malcom Neo <malcom.neo@fe.naval-group.com>
2025-11-10 09:10:30 +0100

    Removing battery system and wind plugin and adding license header
    - Removing battery system and wind plugin. 
    
    - Adding license header
    
    Malcom Neo <malcom.neo@fe.naval-group.com>

--- gui/gz-gui/tutorials/index.md
+++
@@ -1,6 +0,0 @@
-# Introduction 
-
-Gazebo GUI is a component in the Gazebo framework, a set
-of libraries designed to rapidly develop robot applications.
-
-[http://gazebosim.org](http://gazebosim.org)
--- systems/aerial_demo_entity_manager/src/aerial_entity_manager.cpp
+++ systems/aerial_demo_entity_manager/src/aerial_entity_manager.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2025 Naval Group
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * https://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
 #include "custom_aerial_entity_manager/aerial_entity_manager.hpp"
 
 namespace lotusim::gazebo {
