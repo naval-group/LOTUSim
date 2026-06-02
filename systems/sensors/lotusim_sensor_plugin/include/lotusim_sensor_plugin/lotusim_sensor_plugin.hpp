be1b47d4adc208048654826b50ace08124b18261
Esther Rayssiguie <esther.rayssiguie@fe.naval-group.com>
2026-06-02 10:09:18 +0200

    Features/radar sensor
    first radar sensor prototype by IRL CROSSING team.
    
    Malcom Neo <malcom.neo@fe.naval-group.com>
    Esther Rayssiguie <esther.rayssiguie@fe.naval-group.com>
    Kaushik Thirumavalavan <kaushik.thirumavalavan@fe.naval-group.com>

--- systems/sensors/lotusim_sensor_plugin/include/lotusim_sensor_plugin/lotusim_sensor_plugin.hpp
+++ systems/sensors/lotusim_sensor_plugin/include/lotusim_sensor_plugin/lotusim_sensor_plugin.hpp
@@ -42,6 +42,7 @@
 #include "lotusim_common/logger.hpp"
 #include "lotusim_sensor_base/custom_sensor.hpp"
 #include "lotusim_sensor_msgs/msg/collisions.hpp"
+#include "radar_sensor/radar_sensor.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include "subsea_pressure_sensor/subsea_pressure_sensor.hh"
