aeb113772342ef019af89effd4d7ae67226651be
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2026-07-29 16:27:30 +0200

    Updated launch scripts
    This commit comes from a refactoring from Naval Group's people
    
    Signed-off-by: Guillaume Jacquenot <guillaume.jacquenot@gmail.com>

--- launch/entrypoint.sh
+++ launch/entrypoint.sh
@@ -12,7 +12,7 @@ set -e
 
 # Source ROS environment
 source /opt/ros/humble/setup.bash
-source /home/lotusim_ws/install/setup.bash
+source "$HOME/lotusim_ws/install/setup.bash"
 export PATH="/root/.nvm/versions/node/v18.20.8/bin:$PATH"
 
 # Execute the command passed to the container
