6671ecaa7a8e8d19de99aea3640a4fac92b15a41
Guillaume Jacquenot <guillaume.jacquenot@gmail.com>
2026-09-15 21:21:17 +0200

    :fire: Removed unused code
    Signed-off-by: Guillaume Jacquenot <guillaume.jacquenot@gmail.com>

--- systems/physics_engine_interface/include/physics_engine_interface/xdyn_websocket.hpp
+++ systems/physics_engine_interface/include/physics_engine_interface/xdyn_websocket.hpp
@@ -30,16 +30,6 @@ using hdl = websocketpp::connection_hdl;
 using Client = websocketpp::client<websocketpp::config::asio_client>;
 using json = nlohmann::json;
 
-static const gz::math::Quaterniond
-    q_ned_to_enu(0.0, 0.5 * sqrt(2.0), 0.5 * sqrt(2.0), 0.0);
-// Body-frame convention swap (gz FLU <-> xdyn FRD): 180 deg about body-x.
-static const gz::math::Quaterniond q_flu_to_frd(0.0, 1.0, 0.0, 0.0);
-
-gz::math::Quaterniond quatNedToEnu(const gz::math::Quaterniond& q_ned);
-gz::math::Quaterniond quatEnuToNed(const gz::math::Quaterniond& q_enu);
-gz::math::Vector3d vecNedToEnu(const gz::math::Vector3d& v_ned);
-gz::math::Vector3d vecEnuToNed(const gz::math::Vector3d& v_enu);
-
 constexpr unsigned short DEFAULT_WEBSOCKET_TIMEOUT = 5;
 
 /**
